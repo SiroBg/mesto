1ba66394771fa0d5d7aa569f2f1dc69708716bc2
SiroBg <borisusik@yandex.ru>
2021-01-23 02:35:52 +0300

    fix: исправить кнопки по ревью

--- js/script.js
+++ js/script.js
@@ -4,22 +4,34 @@ let profileCloseBtn = profilePopup.querySelector('.popup__close-btn');
 let formElement = profilePopup.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__profile-field_type_name');
 let jobInput = formElement.querySelector('.popup__profile-field_type_job');
+let userName = document.querySelector('.profile__name');
+let userJob = document.querySelector('.profile__job');
 
-profileBtn.addEventListener('click', function(){
+function openPopup() {
   profilePopup.classList.add('popup_opened');
-});
+}
 
-profileCloseBtn.addEventListener('click', function(){
-  profilePopup.classList.remove('popup_opened');
-});
-
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  let userName = document.querySelector('.profile__name');
-  let userJob = document.querySelector('.profile__job');
-  userName.textContent = nameInput.value;
-  userJob.textContent = jobInput.value;
+function closePopup() {
   profilePopup.classList.remove('popup_opened');
 }
+
+function formPlaceHolder() {
+  nameInput.value = userName.textContent;
+  jobInput.value = userJob.textContent;
+  openPopup();
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  userName = document.querySelector('.profile__name');
+  userJob = document.querySelector('.profile__job');
+  userName.textContent = nameInput.value;
+  userJob.textContent = jobInput.value;
+  closePopup();
+}
+
+profileBtn.addEventListener('click', formPlaceHolder);
+
+profileCloseBtn.addEventListener('click', closePopup);
 
 formElement.addEventListener('submit', formSubmitHandler);
